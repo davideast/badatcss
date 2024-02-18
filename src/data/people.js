6daf7ac0973238cb367ce371a35b2166f7523562
David East <deast@google.com>
2024-02-18 17:56:20 +0000

    bad images

---
+++ src/data/people.js
@@ -0,0 +1,22 @@
+export default {
+  "adam": {
+    "name": "adam",
+    "photoUrl": "/people/adam.png"
+  },
+  "david": {
+    "name": "david",
+    "photoUrl": "/people/david.png"
+  },
+  "chris": {
+    "name": "chris",
+    "photoUrl": "/people/chris.png"
+  },
+  "jason": {
+    "name": "jason",
+    "photoUrl": "/people/jason.png"
+  },
+  "dave-rupert": {
+    "name": "dave-rupert",
+    "photoUrl": "/people/dave-rupert.png"
+  }
+}
