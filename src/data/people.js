6de6dd7be267ad68138b817cef71d641b6dc1d22
David East <deast@google.com>
2024-02-27 18:34:57 +0000

    episode cards

--- src/data/people.js
+++ src/data/people.js
@@ -1,22 +1,27 @@
 export default {
   "adam": {
     "name": "adam",
-    "photoUrl": "/people/adam.png"
+    "photoUrl": "/people/adam.png",
+    "twitter": "https://twitter.com/argyleink"
   },
   "david": {
     "name": "david",
-    "photoUrl": "/people/david.png"
+    "photoUrl": "/people/david.png",
+    "twitter": "https://twitter.com/_davideast"
   },
   "chris": {
     "name": "chris",
-    "photoUrl": "/people/chris.png"
+    "photoUrl": "/people/chris.png",
+    "twitter": "https://twitter.com/chriscoyier"
   },
   "jason": {
     "name": "jason",
-    "photoUrl": "/people/jason.png"
+    "photoUrl": "/people/jason.png",
+    "twitter": "https://twitter.com/jlengstorf"
   },
   "dave-rupert": {
     "name": "dave-rupert",
-    "photoUrl": "/people/dave-rupert.png"
+    "photoUrl": "/people/dave-rupert.png",
+    "twitter": "https://twitter.com/davatron5000"
   }
 }
