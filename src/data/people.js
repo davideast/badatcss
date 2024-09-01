4f4f03bcffeed4e0c228cde595b76b160e291132
David East <deast@google.com>
2024-09-01 00:16:37 +0000

    episode

--- src/data/people.js
+++ src/data/people.js
@@ -3,6 +3,8 @@ import david from '../assets/people/david.webp';
 import chris from '../assets/people/chris.webp';
 import jason from '../assets/people/jason.webp';
 import dave from '../assets/people/dave.webp';
+import ericMeyer from '../assets/people/eric-meyer.png';
+import estelleWeyl from '../assets/people/estelle-weyl.png';
 
 export default {
   "adam": {
@@ -29,5 +31,15 @@ export default {
     "name": "dave-rupert",
     "photoUrl": dave.src,
     "twitter": "https://twitter.com/davatron5000"
+  },
+  "eric-meyer": {
+    "name": "eric-meyer",
+    "photoUrl": ericMeyer.src,
+    "twitter": "https://meyerweb.com/"
+  },
+  "estelle-weyl": {
+    "name": "estelle-weyl",
+    "photoUrl": estelleWeyl.src,
+    "twitter": "https://estelle.github.io/"
   }
 }
